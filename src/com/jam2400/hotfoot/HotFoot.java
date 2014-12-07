080c9f9792dc300c625925568b7f4a5173fc25f2
Jam <jamesc2400@gmail.com>
2014-12-07 13:23:51 +0000

    Updating final class.

--- src/com/jam2400/hotfoot/HotFoot.java
+++ src/com/jam2400/hotfoot/HotFoot.java
@@ -1,5 +1,28 @@
 package com.jam2400.hotfoot;
 
-public class HotFoot {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class HotFoot extends JavaPlugin {
+	
+	public void registerListeners(){
+		PluginManager pm;
+		//pm.registerEvents(// TODO events, this);
+	}
+	@Override
+	public void onEnable(){
+		
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		return false;
+	}
 }
